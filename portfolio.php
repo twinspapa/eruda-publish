65df8c034ec00569d297d39f96e775b57c6c9757
kkh <twinspapa@monstercube.kr>
2021-06-30 21:36:14 +0900

    1

--- portfolio.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('views', getcwd().'/.');
-$twig = new \Twig\Environment($loader);
-
-echo $twig->render('portfolio.html', ['name' => 'Fabien']);
